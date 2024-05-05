89672c8fd5c48cf2197a757b94b339435ab86429
ashwanthdurairaj <ashwanthdurairaj2003@gmail.com>
2024-05-05 18:04:53 +0530

    update calculate-winner

--- week3/src/utils/calculate-winner.js
+++ week3/src/utils/calculate-winner.js
@@ -10,15 +10,27 @@ function calculateWinner(squares) {
       [2, 4, 6],
     ];
 
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
+    // for (let i = 0; i < lines.length; i++) {
+    //   const [a, b, c] = lines[i];
 
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+    //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    //     return squares[a];
+    //   }
+    // }
+
+
+    // return null;
+
+    return lines.some((num) => {
+      const [a, b, c] = num
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      {
+        return squares[a]
       }
-    }
-    return null;
+    })
+
+    // return null
 
 }
-
+// console.log(calculateWinner(['X', 'O', 'X', 'O', null, null, null, null, null]))
 export default calculateWinner
