20d3535a26190ca6776001e34133ca17e320577a
ashwanthdurairaj <ashwanthdurairaj2003@gmail.com>
2024-05-07 16:59:04 +0530

    add prev 3 week assignments

---
+++ week1/exercise5.js
@@ -0,0 +1,9 @@
+let grade = 'm'
+
+switch(grade)
+{
+    case 'A': console.log("You got an A, so here's a Chocolate for you!"); break;
+    case 'B': console.log("You got a B, here's a Cookie for you!"); break;
+    case 'C': console.log("You got a C, there's room for improvement and here's your Candy!"); break;
+    default: console.log("Invalid Input")
+}
